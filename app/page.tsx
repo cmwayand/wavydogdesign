6afaf48093d784ef9cced7b8ae3a3cc20dc17ef4
cmwayand <cmwayand@gmail.com>
2025-02-27 21:17:23 -0500

    2

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
           <div className="flex gap-2 items-center text-xl font-bold">
-            <Palette className="h-6 w-6" />
+            <Image src="wavydoglogo.svg" width={24} height={24} alt="Wavy Dog Logo" />
             <span>Wavy Dog Design</span>
           </div>
           <div className="flex flex-1 items-center justify-end space-x-4">
