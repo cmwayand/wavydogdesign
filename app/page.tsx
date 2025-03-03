6306296899a4e7448baf985743f8dd050bc30340
cmwayand <cmwayand@gmail.com>
2025-03-02 19:21:20 -0500

    text

--- app/page.tsx
+++ app/page.tsx
@@ -436,7 +436,7 @@ export default function Home() {
 			<footer className="w-full border-t py-6 md:py-0">
 				<div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
 					<p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-						© {new Date().getFullYear()} Wavy Dog Design. All rights reserved.
+						© {new Date().getFullYear()} Wavy Dog Design LLC. All rights reserved.
 					</p>
 					{/* <div className="flex items-center gap-4 text-sm text-muted-foreground">
 						<Link
